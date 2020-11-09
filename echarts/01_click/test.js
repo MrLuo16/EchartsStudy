8675e1804504b13645180847d70d3577a44d438d
MrLuo <a_20190429@163.com>
2020-11-09 16:38:29 +0800

    添加提示
    添加提示

--- echarts/01_click/test.js
+++ echarts/01_click/test.js
@@ -7,6 +7,9 @@ window.onload = function () {
             data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
         },
         yAxis: {},
+        tooltip:{
+            
+        },
         series: [{
             name: '销量',
             type: 'bar',
